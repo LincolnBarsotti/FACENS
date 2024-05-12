f9b7e2801f449d46f8c84b293c50940345e3a933
Lincoln Barsotti <107706237+LincolnBarsotti@users.noreply.github.com>
2024-05-11 21:27:44 -0300

    update angular project

--- Programação Web/projetos-angular/lista-de-tarefas/src/app/tarefas/tarefas.component.ts
+++ Programação Web/projetos-angular/lista-de-tarefas/src/app/tarefas/tarefas.component.ts
@@ -27,12 +27,19 @@ export class TarefasComponent implements OnInit{
     { descricao: 'Enviar relatório', status: true },
     { descricao: 'Agendar consulta médica', status: false }
   ];
+
+  validarStatus(strg: string){
+    if(strg == "f"){
+      return false
+    }
+    return true;
+  }
   
   criar() {
     if (this.tarefas.descricaoDaTarefa != "" && this.tarefas.selectedStatus != "") {
       let tarefaMomentanea: objetoTarefa = {
         descricao: this.tarefas.descricaoDaTarefa,
-        status: Boolean(this.tarefas.selectedStatus)
+        status: this.validarStatus(this.tarefas.selectedStatus)
       };
 
       this.listaDeTarefas.push(tarefaMomentanea);
@@ -49,19 +56,20 @@ export class TarefasComponent implements OnInit{
     this.listaDeTarefas = this.listaDeTarefas.filter( t => t !== tarefaListada);
   }
 
-}
+  mudarStatusDaTarefa(tarefaListada:objetoTarefa){
+    for (let i = 0; i < this.listaDeTarefas.length; i++) {
+      if(this.listaDeTarefas[i] == tarefaListada){
+        this.listaDeTarefas[i].status = !tarefaListada.status;
+      }
+    }
+  }
 
-/*
-Suponha que você esteja construindo um aplicativo para gerenciar tarefas e deseja exibir uma lista de tarefas concluídas e não concluídas:
-A "Tarefa" terá as seguintes propriedades:
-descrição: string
-concluida: boolean
-No componente que exibe a lista de tarefas, crie um array "tarefas" que contém no mínimo 5 tarefas, sendo algumas concluídas e outras não concluídas.
-Use a diretiva *ngFor para iterar sobre o array "tarefas" e exibir cada tarefa em um elemento HTML <li> em uma lista não ordenada.
-Use a diretiva [ngStyle] para aplicar um estilo de texto tachado (riscado) nas tarefas concluídas na lista.
-Adicione um botão "Concluir" em cada tarefa da lista que alterna o valor da propriedade "concluida" da tarefa entre verdadeiro e falso.
-Dica: Para aplicar um estilo de texto tachado usando a diretiva [ngStyle], você pode definir uma expressão que retorna um objeto com a propriedade "text-decoration" definida como "line-through" se a tarefa estiver concluída
-*/
+  styleObject = {
+    'color': 'green',
+    'font-weight': 'bold'
+  };
+
+}
 interface objetoTarefa {
   descricao: string;
   status: boolean;
